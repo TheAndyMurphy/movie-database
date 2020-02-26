b69f1ebd73cf297babd1cd599bbafe815f96098a
TheAndyMurphy <andrewmurphydublin@gmail.com>
2020-02-26 10:34:14 +0000

    Updated to hooks

--- src/App.js
+++ src/App.js
@@ -1,34 +1,39 @@
-import React from 'react';
+import React, { useState, useEffect }from 'react';
 import './App.scss';
 import Container from './components/container/container.component';
 
-class App extends React.Component {
-  constructor(){
-    super();
-    this.state = {
-        apiKey : process.env.REACT_APP_KEY,
-        movies: [],
-        baseUrl: '',
-        posterSize: ''
-    }
+const App = () => {
+
+  const apiKey = process.env.REACT_APP_KEY;
+  const [movies, setMovies] = useState([]);
+  const [baseUrl, setBaseUrl] = useState('');
+  const [posterSize, setPosterSize] = useState('');
+
+  useEffect(() => {
+    loadConfig();
+    loadData();
+  })
+
+  const loadConfig = async() => {
+    const response = await fetch(`https://api.themoviedb.org/3/configuration?api_key=${apiKey}`);
+    const data = await response.json();
+    setBaseUrl(data.images.base_url);
+    setPosterSize(data.images.poster_sizes[4]);
   }
-  componentDidMount(){
-    fetch(`https://api.themoviedb.org/3/discover/movie?api_key=${this.state.apiKey}&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1`)
-    .then(res => res.json())
-    .then(data => this.setState(prevState => ({...prevState, movies: data.results})))
-    .catch(err => console.log(err));
-    fetch(`https://api.themoviedb.org/3/configuration?api_key=${this.state.apiKey}`)
-    .then(res => res.json())
-    .then(data => this.setState(prevState => ({...prevState, baseUrl: data.images.base_url, posterSize: data.images.poster_sizes[4]})))
+
+  const loadData = async() => {
+    const response = await fetch(`https://api.themoviedb.org/3/discover/movie?api_key=${apiKey}&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1`);
+    const data = await response.json();
+    setMovies(data.results);
   }
-  render(){
-    return (
-      <div className="App">
+
+
+  return(
+    <div className="App">
         <h1>Movie Database</h1>
-        <Container movies={this.state.movies} baseUrl={this.state.baseUrl} posterSize={this.state.posterSize} />
-      </div>
-    );
-  }
+        <Container movies={movies} baseUrl={baseUrl} posterSize={posterSize} />
+    </div>
+  )
 }
 
 export default App;
